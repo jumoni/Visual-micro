8d09a43b9008c65ef4e525d5d9b878e4058caebb
jumoni <liwenf@andrew.cmu.edu>
2019-09-06 23:59:04 -0400

    home page basic

--- visualizer/views.py
+++ visualizer/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+	return render(request, 'home.html', {})
+
+# save data to database
+def save(request):
+	return HttpResponse('Hello, World!')
+
+# get data to draw
+def draw(request):
+	return HttpResponse('Hello, World!')
